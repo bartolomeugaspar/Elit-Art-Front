c1d77738a705b0ef89e851b2911c4a9a55126d11
bartolomeugaspar <bartolomeugasparbg@gmail.com>
2025-11-14 22:45:21 +0100

    Remove static export config to support dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Next.js 14 detecta automaticamente a pasta src
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
   }
